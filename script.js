3f1557642c498ec4dec76dbd3d0348d389178222
amanda <am.morang@gmail.com>
2018-06-16 11:42:05 -0400

    updated js file, added gitignore

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 "use strict";
-let greeting = prompt("Would you like to play a game?");
 
-if (greeting === "yes") {
+{
+    let greeting = prompt("Would you like to play a game?");
+
+if (greeting.toLowerCase() === "yes") {
     let userHealth = 40;
     let grantHealth = 10;
     let userName = prompt("What is your name?");
     let wins = 0;
+    console.log(`Let's Begin! ${userName}'s Starting Health is ${userHealth}, and Grant's Starting Health is ${grantHealth}`);
     while (grantHealth > 0) {
-        console.log(`${userName}'s Health: ${userHealth}`);
-        console.log(`Grant's Health: ${grantHealth}`);
-        console.log(`${userName}'s Health: ${userHealth -= Math.floor(Math.random()* 2 + 1)}`);
-        console.log(`Grant's Health: ${grantHealth -= Math.floor(Math.random() * 2 + 1)}`);
+        console.log(`${userName}'s Health: ${userHealth -= Math.floor((Math.random()* 3) + 1)}`);
+        console.log(`Grant's Health: ${grantHealth -= Math.floor((Math.random() * 3) + 1)}`);
 
-        if (wins >= 3) {
+        if (wins >= 3   || grantHealth <= 0) {
             console.log(`"Congratulations ${userName}! You've won!"`);
             break;
         } else if (userHealth <= 0) {
@@ -25,5 +26,6 @@ if (greeting === "yes") {
         }
     }
 } else if (greeting === "no") {
-    console.log("Maybe next time!");
+    console.log("Maybe next time?");
+    }
 }
