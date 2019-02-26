bcd30e4b157f8002cd570cae043c5b238fbbfb9d
amanda <am.morang@gmail.com>
2019-02-25 20:50:09 -0500

    added functionality so user must enter a name, and to remove errors in console if user clicks cancel during the game

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
     let greeting = prompt("Would you like to play a game?");
 
     if (greeting === null) {
-        console.log("Aww, not even once?")
+        console.log("Aww, not even once?");
     } else if (greeting.toLowerCase() === "yes") {
         let userHealth = 40;
         let grantHealth = 10;
@@ -12,8 +12,10 @@
         while (!userName) {
             userName = prompt("Please enter your name");
         }
+
         let wins = 0;
         console.log(`Let's Begin! ${userName}'s Starting Health is ${userHealth}, and Grant's Starting Health is ${grantHealth}`);
+        
         while (grantHealth > 0) {
             console.log(`${userName}'s Health: ${userHealth -= Math.floor((Math.random()* 3) + 1)}`);
             console.log(`Grant's Health: ${grantHealth -= Math.floor((Math.random() * 3) + 1)}`);
